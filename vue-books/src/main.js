6cbe16edfb717158f7a944083383913dad16995e
blind181 <blind181@126.com>
2017-06-13 21:58:22 +0800

    books stuff

--- vue-books/src/main.js
+++ vue-books/src/main.js
@@ -7,12 +7,25 @@ import Explorer from './Explorer.vue'
 import Cart from './Cart.vue'
 import Me from './Me.vue'
 
-//
+//使用路由实例插件
 Vue.use(VueRouter)
 
+const router = new VueRouter({
+    mode: 'history',
+    baseline: _dirname,
+    routes: [
+        //将页面组件与path指定路由关联
+        {path: '/home', conponent: Home},
+        {path: '/explorer', conponent: Explorer},
+        {path: '/cart', conponent: Cart},
+        {path: '/me', conponent: Me},
+    ]
+})
 
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    //将路由实例添加到Vue实例中
+    router,
+    render: h => h(App)
 })
